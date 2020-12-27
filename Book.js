715ab1a9efb67f0d2490c81f244ca2eae2ddd944
Jairo <jairoh28@gmail.com>
2020-12-27 14:52:12 -0600

    Initial commit

---
+++ Book.js
@@ -0,0 +1,25 @@
+class Book {
+  constructor(title, author, isbn) {
+  	this.title = title;
+  	this.author = author;
+  	this.isbn = isbn;
+  	this.patron = null;
+  	this.dueDate = null;
+  	this._out = false;
+  }
+
+  set out(out) {
+  	this._out = out;
+  	if (out) {
+  		const newDueDate = new Date();
+  		newDueDate.setDate(newDueDate.getDate() + 14);
+  		this.dueDate = newDueDate;
+  	} else {
+  		this.dueDate = null;
+  	}
+  }
+
+  get out() {
+	return this._out;
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Overview
+
+1) In the Library.js file, the Library class has a constructor method. In the constructor method, books & patrons are initialized.
+
+2) In the Book.js file, the Book class has a constructor method. In the constructor method, title, author, & isbn are initialized.
+
+3) In the Patron.js file, the Patron class has a constructor method. In the constructor method, name, email & currentBook are initialized.
